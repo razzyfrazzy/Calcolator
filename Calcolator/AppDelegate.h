52ecb19c5ce1a941740e640c63fcd88596ccd95d
razzyfrazzy <raizaab@me.com>
2015-05-04 20:58:42 -0700

    Initial Commit

---
+++ Calcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calcolator
+//
+//  Created by Raiza Abubakar on 5/4/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calcolator
+//
+//  Created by Raiza Abubakar on 5/4/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
